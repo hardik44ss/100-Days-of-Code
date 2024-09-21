511c09502a54376895715bb9100f9f3ffbd1e834
Harbansh <hardiksuthar4444@gmail.com>
2024-09-21 21:13:34 +0530

    Day34

--- Day 34/04_addTwoNumber.cpp
+++ Day 34/04_addTwoNumber.cpp
@@ -15,18 +15,18 @@ public:
 };
 
 Node* addTwoNumbers(Node* l1, Node* l2) {
-        Node* dummy = new ListNode(0); // dummy node to simplify the code
+        Node* dummy = new Node(0); // dummy node to simplify the code
         Node* current = dummy;
         int carry = 0;
 
         while (l1 != nullptr || l2 != nullptr || carry != 0) {
             int sum = carry;
             if (l1 != nullptr) {
-                sum += l1->val;
+                sum += l1->data;
                 l1 = l1->next;
             }
             if (l2 != nullptr) {
-                sum += l2->val;
+                sum += l2->data;
                 l2 = l2->next;
             }
             carry = sum / 10;
@@ -59,13 +59,13 @@ int main() {
     l2->next = new Node(6);
     l2->next->next = new Node(4);
 
-    print("List 1 : ");
+    cout<<"List1 : ";
     printList(l1);
-    print("List 2 : ");
+    cout<<"List2 : ";
     printList(l2);
 
     Node* result = addTwoNumbers(l1, l2);
-    print("Result : ");
+    cout<<"Result : ";
     printList(result);
 
     return 0;
