62480df4ef9e28ce9be4fe89c33c38eb35e94c19
Harbansh <hardiksuthar4444@gmail.com>
2024-10-11 22:29:20 +0530

    Day55

--- Day 53/kthLargest.cpp
+++ Day 53/kthLargest.cpp
@@ -1,19 +1,34 @@
+#include <vector>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        
+        // Min-heap to store the k largest elements
         priority_queue<int, vector<int>, greater<int>> pq;
 
-        for(auto i : nums)
-        {
-            pq.push(i);
-            if( pq.size() > k )
-            {
+        // Iterate through all elements in the array
+        for(const int& num : nums) {
+            pq.push(num);
+            // If the size of the heap exceeds k, remove the smallest element
+            if(pq.size() > k) {
                 pq.pop();
             }
         }
 
+        // The top element of the heap is the k-th largest element
         return pq.top();
-        
     }
 };
+#include <iostream>
+
+int main() {
+    Solution sol;
+    vector<int> nums = {3, 2, 1, 5, 6, 4};
+    int k = 2;
+    cout << "The " << k << "th largest element is " << sol.findKthLargest(nums, k) << endl;
+    return 0;
+}
